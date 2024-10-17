65f5046b19c43db19121fe5f174cbc7b0862dff4
El Azhari <Abdel Wahab>
2024-10-17 23:17:02 +0200

    new:2.2

---
+++ exercices/2.2-2-3-4/Cinema/src/App.tsx
@@ -0,0 +1,78 @@
+type MovieProps = {
+  title: string;
+  director: string;
+};
+
+
+type CinemaProps = {
+  name: string;
+  movie1:MovieProps;
+  movie2:MovieProps;
+  
+};
+
+
+
+
+
+const Cinema = (props: CinemaProps) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+     <p>{props.movie1.title}</p>
+     <p>{props.movie1.director}</p>
+     <p>{props.movie2.title}</p>
+     <p>{props.movie2.director}</p>
+      
+    </div>
+  );
+};
+
+interface PageTitleProps {
+  title: string;
+}
+
+const PageTitle = (props: PageTitleProps) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+    </div>
+  );
+};
+
+const App = () => {
+  const pageTitle = "Informations sur les films dans les cinémas";
+
+  const cinema1Name = "UGC DeBrouckère";
+
+  const movie1:MovieProps = {
+    title: "HAIKYU-THE DUMPSTER BATTLE",
+    director: "Susumu Mitsunaka",
+  };
+  const movie2:MovieProps = {
+    title: "GOODBYE JULIA ",
+    director: "Mohamed Kordofani",
+  };
+
+  const cinema2Name = "UGC Toison d'Or";
+  const movie3:MovieProps = {
+    title: "THE WATCHERS",
+    director: "Ishana Night Shyamalan",
+  };
+  const movie4:MovieProps = {
+    title: "BAD BOYS: RIDE OR DIE",
+    director: "Adil El Arbi, Bilall Fallah",
+  };
+
+  return (
+    <div>
+      <PageTitle title={pageTitle} />
+
+      <Cinema name={cinema1Name} movie1={movie1} movie2={movie2} />
+
+      <Cinema name={cinema2Name} movie1={movie3} movie2={movie4} />
+    </div>
+  );
+};
+
+export default App;
