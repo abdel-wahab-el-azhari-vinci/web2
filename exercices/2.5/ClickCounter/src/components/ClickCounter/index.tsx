394e48675ed9a556b1b42000a92459d6818e8f16
El Azhari <Abdel Wahab>
2024-10-25 14:51:24 +0200

    new:ex2.5-destructuring

--- exercices/2.5/ClickCounter/src/components/ClickCounter/index.tsx
+++ exercices/2.5/ClickCounter/src/components/ClickCounter/index.tsx
@@ -2,20 +2,26 @@
 import { useState } from "react";
 import "../App/App.css";
 
-/* c'est le clickCounter */
+interface ClickCounterProps {
+    title: string;
+    message: string;
+  }
   
-  const ClickCounter = () => {
+  const ClickCounter = ({ title, message }: ClickCounterProps) => {
     const [count, setCount] = useState(0);
-    
+    let valeur = false;
+    if(count>=10){
+      valeur=true;
+    }
     return (
       <div>
-       
+        <h4>{title}</h4>
         <div className="card">
           <button onClick={() => setCount((count) => count + 1)}>
             count is {count}
           
           </button>
-         
+          <p>{valeur ? `${message} `: "pas encore expert"}</p>
           <p>
             Edit <code>src/App.tsx</code> and save to test HMR
           </p>
