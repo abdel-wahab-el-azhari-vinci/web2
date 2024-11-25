a158864130a1dd178e7f16cd3064f7401d718b87
El Azhari <Abdel Wahab>
2024-11-25 21:48:54 +0100

    add: new 2.13

---
+++ exercices/2.13-14/Joke/src/components/App.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import "./App/App.css";
+
+interface Joke {
+  category: string;
+  joke: string;
+}
+
+function App() {
+  const [jokes, setJokes] = useState<Joke[]>([]);
+
+  useEffect(() => {
+    fetch("https://v2.jokeapi.dev/joke/Programming?type=single")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `fetch error : ${response.status} : ${response.statusText}`
+          );
+        }
+        return response.json();
+      })
+      .then((data: Joke) => {
+        setJokes([data]);
+      })
+      .catch((err) => {
+        console.error("HomePage::error: ", err);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>The joke</h2>
+      {jokes.map((joke) => (
+        <div>
+          <p>The joke : {joke.joke}</p>
+          <p>The category : {joke.category}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
